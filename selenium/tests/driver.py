bedc53e1df28ae0fa52fd6736011068cb6893fa2
MuhammadLazaro <muhammadlazaro21@gmail.com>
2026-02-23 13:37:32 +0700

    Full CI fix for selenium docker network

--- selenium/tests/driver.py
+++ selenium/tests/driver.py
@@ -1,2 +1,16 @@
-# This file is kept for backward compatibility
-# All driver setup is now in conftest.py
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def create_driver():
+    options = Options()
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+
+    driver = webdriver.Remote(
+        command_executor="http://selenium:4444/wd/hub",
+        options=options
+    )
+
+    driver.implicitly_wait(10)
+    return driver
