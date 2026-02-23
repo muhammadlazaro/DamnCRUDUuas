b1c1029fcc629987bcb9370a2c7b6782f34ddfda
MuhammadLazaro <muhammadlazaro21@gmail.com>
2026-02-23 12:05:52 +0700

    Fix: Add webdriver-manager dan simplify pytest command

--- DamnCRUD-uas2026/selenium/tests/driver.py
+++ DamnCRUD-uas2026/selenium/tests/driver.py
@@ -1,9 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 def create_driver():
     options = Options()
     options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    return webdriver.Chrome(options=options)
+    
+    service = Service(ChromeDriverManager().install())
+    return webdriver.Chrome(service=service, options=options)
