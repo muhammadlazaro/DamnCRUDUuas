6eae09589f3dcc9bd88a8972be64199bbb75bf6a
MuhammadLazaro <muhammadlazaro21@gmail.com>
2026-02-23 12:56:35 +0700

    Fix CI for docker compose selenium

--- selenium/tests/driver.py
+++ selenium/tests/driver.py
@@ -5,9 +5,13 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 def create_driver():
     options = Options()
-    options.add_argument("--headless=new")
+    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    
-    service = Service(ChromeDriverManager().install())
-    return webdriver.Chrome(service=service, options=options)
+
+    driver = webdriver.Remote(
+        command_executor="http://selenium:4444/wd/hub",
+        options=options
+    )
+
+    return driver
